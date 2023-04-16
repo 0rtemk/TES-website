5828a7452751ab949429a6fef9454d0a20aca6d2
AlexasandrYakovlef <aleksandar.iack0vlev@mail.ru>
2023-04-16 22:19:56 +0300

    fix position of text

--- start_page/src/components/Main/ForStudents.js
+++ start_page/src/components/Main/ForStudents.js
@@ -12,12 +12,12 @@ const ForStudents = () => {
             <section name="ForStudents" id="ForStudents" className="py-5">
                 <div className="fs-1"><strong>Что получает участник</strong></div>
                 <div className="mission padding-section">
-                    <div className="section-check text-start">
+                    <div className="row section-check text-start">
                         <div className="row py-4">
                             <div className="col-2" align="center">
                                 <img src={fs_1} alt="" className="w-img w-80"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-10">
                                 <div className="fs-3 py-2 lh-1"><strong>Готовый к реализации стартап</strong></div>
                                 <div className="fs-5">После прохождения всех этапов обучения – готовый бизнес-проект для представления инвесторам</div>
                             </div>
@@ -26,7 +26,7 @@ const ForStudents = () => {
                             <div className="col-2" align="center">
                                 <img src={fs_2} alt="" className="w-img w-80"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-10">
                                 <div className="fs-3 py-2 lh-1"><strong>Взаимодействие с резидентами ОЭЗ «Дубна»</strong></div>
                                 <div className="fs-5">Участник получит возможность найти работу у резидентов ОЭЗ «Дубна» и предприятий научно-производственного комплекса города</div>
                             </div>
@@ -35,7 +35,7 @@ const ForStudents = () => {
                             <div className="col-2" align="center">
                                 <img src={fs_3} alt="" className="w-img w-80"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-10">
                                 <div className="fs-3 py-2 lh-1"><strong>Денежные средства на начальный этап реализации проекта</strong></div>
                                 <div className="fs-5">Победителям Хакатона «Цифра.zоne | Технологическая модернизация» 2.0 предоставляется денежный приз</div>
                             </div>
@@ -44,7 +44,7 @@ const ForStudents = () => {
                             <div className="col-2" align="center">
                                 <img src={fs_4} alt="" className="w-img w-60"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-10">
                                 <div className="fs-3 py-2 lh-1"><strong>Сертификаты об обучении Университета «Дубна» и Сколково</strong></div>
                                 <div className="fs-5">После каждого курса обучения участникам предоставляются сертификаты о прохождении обучения</div>
                             </div>
@@ -53,7 +53,7 @@ const ForStudents = () => {
                             <div className="col-2" align="center">
                                 <img src={fs_5} alt="" className="w-img w-80"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-10">
                                 <div className="fs-3 py-2 lh-1"><strong>Диплом о профессиональной переподготовке «Менеджмент в области управления проектами»</strong></div>
                                 <div className="fs-5">При успешном освоении всех модулей программы</div>
                             </div>
--- start_page/src/components/Main/Mission.js
+++ start_page/src/components/Main/Mission.js
@@ -9,12 +9,12 @@ const Mission = () => {
                 <div className="fs-1"><strong>Миссия школы</strong></div>
                 <div className="fs-2 py-2">Подготовка технолидеров</div>
                 <div className="mission padding-section">
-                    <div className="section-check text-start">
+                    <div className="row section-check text-start">
                         <div className="row py-4">
                             <div className="col-1">
                                 <img src={checked} alt="" className="w-img w-40"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-11">
                                 <div className="fs-3 py-2 lh-1"><strong>Вовлечение в технологическое предпринимательство</strong></div>
                                 <div className="fs-5">Помощь молодым талантам в получении прибыли от реализации инженерно-технических идей, знакомство участников с площадками резидентов ОЭЗ «Дубна»</div>
                             </div>
@@ -23,7 +23,7 @@ const Mission = () => {
                             <div className="col-1">
                                 <img src={checked} alt="" className="w-img w-40"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-11">
                                 <div className="fs-3 py-2 lh-1"><strong>Удовлетворение кадровой потребности резидентов ОЭЗ «Дубна» и научно-производственного комплекса города</strong></div>
                                 <div className="fs-5">Возможность поработать с действующими предприятиями и решить реальные задачи</div>
                             </div>
@@ -32,7 +32,7 @@ const Mission = () => {
                             <div className="col-1">
                                 <img src={checked} alt="" className="w-img w-40"></img>
                             </div>
-                            <div className="col">
+                            <div className="col-11">
                                 <div className="fs-3 py-2 lh-1"><strong>Образование</strong></div>
                                 <div className="fs-5">Навыки управления проектами</div>
                             </div>
