ed8ff03fb50b5f6bcd253482b95cdb3ccc6f8fad
0rtemk <lakeylive@yandex.ru>
2023-04-24 12:57:26 +0300

    add cors to index.js

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const tasks = require('./routes/authRoutes')
 const connectDB = require('./db/connect')
 const authRouter = require('./routes/authRoutes')
 const port = process.env.PORT || 5000
+const cors = require('cors')
 require('dotenv').config()
 
 app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}))
 app.use('/auth', authRouter)
 
 const start = async () => {
