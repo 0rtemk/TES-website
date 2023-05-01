e4a8d46d3b3f707291869d5da2f4af9aea15a13c
Kirillkuk <khryavin@list.ru>
2023-05-01 22:20:19 +0300

    Created EventRoutes

---
+++ routes/EventRoutes.js
@@ -0,0 +1,34 @@
+const Router = require('express')
+const router = new Router()
+const controller = require('../controllers/EventsController')
+const { check } = require('express-validator')
+
+router.post(
+  '/createEvent',
+  [check('event_name', 'Название мероприятия слишком маленькое').isLength({ min: 5}),
+   check('event_info', 'Описание мероприятия не может быть пустым').notEmpty(),
+   check('event_direction', 'Направление мероприятия не может быть пустым').notEmpty(),
+   check('event_skills', 'Навыки мероприятия не могут быть пустыми').notEmpty(),
+   check('event_start', 'Дата начала мероприятия не может быть пустой').notEmpty(),
+   check('event_finish', 'Дата конца мероприятия не может быть пустой').notEmpty(),
+  ],
+  controller.createEvent
+)
+
+router.post(
+  '/updateEvent',
+  [check('event_name', 'Название мероприятия слишком маленькое').isLength({ min: 5}),
+   check('event_info', 'Описание мероприятия не может быть пустым').notEmpty(),
+   check('event_direction', 'Направление мероприятия не может быть пустым').notEmpty(),
+   check('event_skills', 'Навыки мероприятия не могут быть пустыми').notEmpty(),
+   check('event_start', 'Дата начала мероприятия не может быть пустой').notEmpty(),
+   check('event_finish', 'Дата конца мероприятия не может быть пустой').notEmpty(),
+  ],
+  controller.updateEvent
+)
+
+router.delete('/deleteEvent', controller.deleteEvent)
+
+router.get('/allEvents', controller.GetAllEvents)
+
+module.exports = router
